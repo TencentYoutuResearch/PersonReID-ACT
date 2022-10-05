26b61163d63ab1ca8c8c9915804ca51368d5f943
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 01:07:55 +0000

    Adding numeric_owner as keyword arguement

--- reid/datasets/msmt17.py
+++ reid/datasets/msmt17.py
@@ -63,7 +63,7 @@ class msmt17(Dataset):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, raw_dir)
